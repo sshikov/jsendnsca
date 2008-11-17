003a2c0b1ad31bea63209d754395f3afbdf777bd
rajneeshpatel <rajneeshpatel@d719a9fe-b3ce-11dd-b408-c1c371648c19>
2008-11-17 09:41:56 +0000

    added javadoc comments

--- src/main/java/com/googlecode/jsendnsca/sender/INagiosPassiveCheckSender.java
+++ src/main/java/com/googlecode/jsendnsca/sender/INagiosPassiveCheckSender.java
@@ -1,6 +1,18 @@
 package com.googlecode.jsendnsca.sender;
 
+/**
+ * Interface to be implemented by {@link NagiosPassiveCheckSender}'s
+ * 
+ * @author Raj.Patel
+ * @version 1.0
+ */
 public interface INagiosPassiveCheckSender 
 {
+	/**
+	 * Send Passive Check
+	 * 
+	 * @param payload the Passive Check message payload
+	 * @throws NagiosException thrown if an error occurs while sending the passive check
+	 */
 	void send(MessagePayload payload) throws NagiosException;
 }
