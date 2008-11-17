c80c9b93c622121842319b517fb6d22eb0286019
rajneeshpatel <rajneeshpatel@d719a9fe-b3ce-11dd-b408-c1c371648c19>
2008-11-17 09:57:32 +0000

    added javadoc comments

--- trunk/src/main/java/com/googlecode/jsendnsca/cli/UsageException.java
+++ trunk/src/main/java/com/googlecode/jsendnsca/cli/UsageException.java
@@ -6,27 +6,37 @@ import java.io.StringWriter;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 
+/**
+ * Thrown if invalid or missing command line arguments are provided
+ * 
+ * @author Raj.Patel
+ * @version 1.0
+ */
 public class UsageException extends Exception {
 
     private static final long serialVersionUID = 6391805034995467681L;
     private static final String COMMAND_LINE_SYNTAX = "jsend-nsca [OPTIONS] OK|WARNING|CRITICAL servicename message";
 
-    private String message;
+    private String usageInfo;
 
-    @Override
-    public String getMessage() {
-        return message;
-    }
-
+    /**
+     * Construct a new {@link UsageException} with the parsed {@link Options}
+     * 
+     * @param options
+     *            the parsed {@link Options}
+     */
     public UsageException(Options options) {
         StringWriter stringWriter = new StringWriter();
 
         new HelpFormatter().printHelp(new PrintWriter(stringWriter), 100, COMMAND_LINE_SYNTAX, "[OPTIONS]", options, 2, 10, "", false);
 
-        message = stringWriter.toString();
+        usageInfo = stringWriter.toString();
     }
 
+    /**
+     * Print formatted usage information on the command line syntax
+     */
     public void printUsageInfo() {
-        System.out.print(message);
+        System.out.print(usageInfo);
     }
 }
