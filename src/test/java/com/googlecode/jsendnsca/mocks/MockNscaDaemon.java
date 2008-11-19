f1ff2c6f572857273791a1c24e9c27f29b8aadd1
rajneeshpatel <rajneeshpatel@d719a9fe-b3ce-11dd-b408-c1c371648c19>
2008-11-19 21:56:17 +0000

    tidy up code

--- src/test/java/com/googlecode/jsendnsca/mocks/MockNscaDaemon.java
+++ src/test/java/com/googlecode/jsendnsca/mocks/MockNscaDaemon.java
@@ -8,29 +8,29 @@ import java.net.Socket;
 import java.util.Date;
 
 public class MockNscaDaemon implements Runnable {
+	private static final int PASSIVE_CHECK_SIZE = 720;
 	private static final int NSCA_PORT = 5667;
 	private static final int INITIALISATION_VECTOR_SIZE = 128;
 
 	public void run() {
-
 		try {
-			ServerSocket serverSocket = null;
-			serverSocket = new ServerSocket(NSCA_PORT);
-			Socket clientSocket = null;
-			clientSocket = serverSocket.accept();
+			ServerSocket serverSocket = new ServerSocket(NSCA_PORT);
+			Socket clientSocket = serverSocket.accept();
 
 			DataOutputStream outputStream = new DataOutputStream(clientSocket.getOutputStream());
 			DataInputStream inputStream = new DataInputStream(clientSocket.getInputStream());
 
-			// write init vector
+			// write init vector and timestamp
 			byte[] initVector = new byte[INITIALISATION_VECTOR_SIZE];
 			outputStream.write(initVector);
 			outputStream.writeInt((int) new Date().getTime());
 			outputStream.flush();
 
 			// read passive check bytes
-			byte[] passiveCheckBytes = new byte[720];
-			inputStream.readFully(passiveCheckBytes, 0, 720);
+			byte[] passiveCheckBytes = new byte[PASSIVE_CHECK_SIZE];
+			inputStream.readFully(passiveCheckBytes, 0, PASSIVE_CHECK_SIZE);
+			
+			//TODO: Check all parts are sent OK
 
 			clientSocket.close();
 			serverSocket.close();
