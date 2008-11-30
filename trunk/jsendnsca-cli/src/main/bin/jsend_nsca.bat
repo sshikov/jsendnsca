f9f3fb4c371a094ee775a53d8fbc18c5a77ab440
rajneeshpatel <rajneeshpatel@d719a9fe-b3ce-11dd-b408-c1c371648c19>
2008-11-30 11:30:43 +0000

    issue 5: updated scripts with new jar name

--- trunk/jsendnsca-cli/src/main/bin/jsend_nsca.bat
+++ trunk/jsendnsca-cli/src/main/bin/jsend_nsca.bat
@@ -1,4 +1,9 @@
 @echo off
-setlocal
-java -jar jsendnsca-jar-with-dependencies.jar %*
-endlocal
+
+set CLASSPATH=jsendnsca-cli.jar
+set CLASSPATH=%CLASSPATH%;../lib/jsendnsca-core.jar
+set CLASSPATH=%CLASSPATH%;../lib/commons-cli-1.1.jar
+set CLASSPATH=%CLASSPATH%;../lib/commons-lang-2.4.jar
+set CLASSPATH=%CLASSPATH%;../lib/commons-io-1.4.jar
+
+java -classpath %CLASSPATH% com.googlecode.jsendnsca.Main %*
