3945b5d4281aef719b8ee8c209bdee8e033513c4
rajneeshpatel <rajneeshpatel@d719a9fe-b3ce-11dd-b408-c1c371648c19>
2008-11-30 15:06:28 +0000

    issue 5: version now in jar name

--- trunk/jsendnsca-cli/src/main/bin/jsend_nsca.bat
+++ trunk/jsendnsca-cli/src/main/bin/jsend_nsca.bat
@@ -1,7 +1,7 @@
 @echo off
 
-set CLASSPATH=jsendnsca-cli.jar
-set CLASSPATH=%CLASSPATH%;../lib/jsendnsca-core.jar
+set CLASSPATH=jsendnsca-cli-1.1.jar
+set CLASSPATH=%CLASSPATH%;../lib/jsendnsca-core-1.1.jar
 set CLASSPATH=%CLASSPATH%;../lib/commons-cli-1.1.jar
 set CLASSPATH=%CLASSPATH%;../lib/commons-lang-2.4.jar
 set CLASSPATH=%CLASSPATH%;../lib/commons-io-1.4.jar
