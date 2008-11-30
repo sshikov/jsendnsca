6ece94fe7777358cbcf0f8892007233ddd85117d
rajneeshpatel <rajneeshpatel@d719a9fe-b3ce-11dd-b408-c1c371648c19>
2008-11-30 15:12:35 +0000

    added socket connection timeout

--- jsendnsca-core/src/main/java/com/googlecode/jsendnsca/core/NagiosSettings.java
+++ jsendnsca-core/src/main/java/com/googlecode/jsendnsca/core/NagiosSettings.java
@@ -10,7 +10,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 package com.googlecode.jsendnsca.core;
 
 import org.apache.commons.lang.StringUtils;
@@ -31,97 +31,101 @@ public class NagiosSettings {
 	 * XOR encryption
 	 */
 	public static final int XOR_ENCRYPTION = 1;
-    
+
 	private String nagiosHost = "localhost";
-    private int port = 5667;
-    private String password = "hasturrocks";
-    private int timeout = 10000;
-    private int encryptionMethod = XOR_ENCRYPTION;
+	private int port = 5667;
+	private String password = "hasturrocks";
+	private int timeout = 10000;
+	private int connectTimeout = 5000;
+	private int encryptionMethod = XOR_ENCRYPTION;
 
-    /**
-     * The host or IP of the Nagios host running the NSCA add-on
-     * 
-     * @return the host or IP, defaults to localhost
-     */
-    public String getNagiosHost() {
-        return nagiosHost;
-    }
+	/**
+	 * The host or IP of the Nagios host running the NSCA add-on
+	 * 
+	 * @return the host or IP, defaults to localhost
+	 */
+	public String getNagiosHost() {
+		return nagiosHost;
+	}
 
-    /**
-     * The host or IP of the Nagios host running the NSCA add-on
-     * 
-     * @param nagiosHost
-     *            the host or IP, defaults to localhost
-     */
-    public void setNagiosHost(String nagiosHost) {
-        if(StringUtils.isBlank(nagiosHost)) {
-            throw new IllegalArgumentException("nagiosHost cannot be null or empty");
-        }
-        this.nagiosHost = nagiosHost;
-    }
+	/**
+	 * The host or IP of the Nagios host running the NSCA add-on
+	 * 
+	 * @param nagiosHost
+	 *            the host or IP, defaults to localhost
+	 */
+	public void setNagiosHost(String nagiosHost) {
+		if (StringUtils.isBlank(nagiosHost)) {
+			throw new IllegalArgumentException(
+					"nagiosHost cannot be null or empty");
+		}
+		this.nagiosHost = nagiosHost;
+	}
 
-    /**
-     * The port on which NSCA is listening
-     * 
-     * @return the port, defaults to 5667
-     */
-    public int getPort() {
-        return port;
-    }
+	/**
+	 * The port on which NSCA is listening
+	 * 
+	 * @return the port, defaults to 5667
+	 */
+	public int getPort() {
+		return port;
+	}
 
-    /**
-     * The port on which NSCA is listening
-     * 
-     * @param port
-     *            the port, defaults to 5667
-     */
-    public void setPort(int port) {
-        this.port = port;
-    }
+	/**
+	 * The port on which NSCA is listening
+	 * 
+	 * @param port
+	 *            the port, defaults to 5667
+	 */
+	public void setPort(int port) {
+		this.port = port;
+	}
 
-    /**
-     * The password configured in the ncsa.cfg file used by NSCA
-     * 
-     * @return the password, defaults to "hasturrocks"
-     */
-    public String getPassword() {
-        return password;
-    }
+	/**
+	 * The password configured in the ncsa.cfg file used by NSCA
+	 * 
+	 * @return the password, defaults to "hasturrocks"
+	 */
+	public String getPassword() {
+		return password;
+	}
 
-    /**
-     * The password configured in the ncsa.cfg file used by NSCA
-     * 
-     * @param password
-     *            the password, defaults to "hasturrocks"
-     */
-    public void setPassword(String password) {
-        if(StringUtils.isBlank(password)) {
-            throw new IllegalArgumentException("password cannot be null or empty");
-        }
-        this.password = password;
-    }
+	/**
+	 * The password configured in the ncsa.cfg file used by NSCA
+	 * 
+	 * @param password
+	 *            the password, defaults to "hasturrocks"
+	 */
+	public void setPassword(String password) {
+		if (StringUtils.isBlank(password)) {
+			throw new IllegalArgumentException(
+					"password cannot be null or empty");
+		}
+		this.password = password;
+	}
 
-    /**
-     * The socket timeout to use when sending the passive check
-     * 
-     * @return the timeout in ms, defaults to 10000 ms
-     */
-    public int getTimeout() {
-        return timeout;
-    }
+	/**
+	 * The socket timeout to use when sending the passive check
+	 * 
+	 * @return the timeout in ms, defaults to 10000 ms
+	 */
+	public int getTimeout() {
+		return timeout;
+	}
 
-    /**
-     * The socket timeout to use when sending the passive check
-     * 
-     * @param timeout
-     *            the timeout in ms, defaults to 10000 ms
-     */
-    public void setTimeout(int timeout) {
-        this.timeout = timeout;
-    }
+	/**
+	 * The socket timeout to use when sending the passive check
+	 * 
+	 * @param timeout
+	 *            the timeout in ms, defaults to 10000 ms
+	 */
+	public void setTimeout(int timeout) {
+		this.timeout = timeout;
+	}
 
 	/**
 	 * The encryption method used by the NSCA Daemon
+	 * 
 	 * @return the method
 	 */
 	public int getEncryptionMethod() {
@@ -129,13 +133,31 @@ public class NagiosSettings {
 	}
 
 	/**
-	 * The encryption method used by the NSCA Daemon, currently only NO_ENCRYPTION or XOR_ENCRYPTION
-	 * @param encryptionMethod the method
+	 * The encryption method used by the NSCA Daemon, currently only
+	 * NO_ENCRYPTION or XOR_ENCRYPTION
+	 * 
+	 * @param encryptionMethod
+	 *            the method
 	 */
 	public void setEncryptionMethod(int encryptionMethod) {
-		if(encryptionMethod != NO_ENCRYPTION && encryptionMethod != XOR_ENCRYPTION) {
-			throw new UnsupportedOperationException("Currently only NO_ENCRYPTION or XOR_ENCRYPTION Supported");
+		if (encryptionMethod != NO_ENCRYPTION
+				&& encryptionMethod != XOR_ENCRYPTION) {
+			throw new UnsupportedOperationException(
+					"Currently only NO_ENCRYPTION or XOR_ENCRYPTION Supported");
 		}
 		this.encryptionMethod = encryptionMethod;
+	}
+
+	/**
+	 * The connection timeout
+	 * 
+	 * @return timeout in ms
+	 */
+	public int getConnectTimeout() {
+		return connectTimeout;
+	}
+
+	public void setConnectTimeout(int connectTimeout) {
+		this.connectTimeout = connectTimeout;
 	}
 }
