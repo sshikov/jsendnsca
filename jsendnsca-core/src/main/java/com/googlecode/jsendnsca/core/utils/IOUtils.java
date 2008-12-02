c5a3cc9a51be76f24e800aa18cea190af7fd1b09
rajneeshpatel <rajneeshpatel@d719a9fe-b3ce-11dd-b408-c1c371648c19>
2008-12-02 09:44:48 +0000

    issue 6: removed commons-io by bring the methods into IOUtils.java

---
+++ jsendnsca-core/src/main/java/com/googlecode/jsendnsca/core/utils/IOUtils.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.jsendnsca.core.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class IOUtils {
+	
+	/**
+	 * Unconditionally close an <code>InputStream</code>.
+	 * <p>
+	 * Equivalent to {@link InputStream#close()}, except any exceptions will be
+	 * ignored. This is typically used in finally blocks.
+	 * 
+	 * @param input
+	 *            the InputStream to close, may be null or already closed
+	 */
+	public static void closeQuietly(InputStream input) {
+		try {
+			if (input != null) {
+				input.close();
+			}
+		} catch (IOException ignore) {
+		}
+	}
+
+	/**
+	 * Unconditionally close an <code>OutputStream</code>.
+	 * <p>
+	 * Equivalent to {@link OutputStream#close()}, except any exceptions will be
+	 * ignored. This is typically used in finally blocks.
+	 * 
+	 * @param output
+	 *            the OutputStream to close, may be null or already closed
+	 */
+	public static void closeQuietly(OutputStream output) {
+		try {
+			if (output != null) {
+				output.close();
+			}
+		} catch (IOException ignore) {
+		}
+	}
+}
