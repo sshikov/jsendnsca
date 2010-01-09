8d25a307f73d886b9de0fc6b1284e52088257685
rajneeshpatel <rajneeshpatel@d719a9fe-b3ce-11dd-b408-c1c371648c19>
2010-01-09 16:00:33 +0000

    branching 1.2 codebase in preparation for 2.0

--- jsendnsca-cli/src/main/bin/jsend_nsca.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set CLASSPATH=jsendnsca-cli-@version@.jar
-set CLASSPATH=%CLASSPATH%;../lib/jsendnsca-core-@version@.jar
-set CLASSPATH=%CLASSPATH%;../lib/commons-cli-1.1.jar
-set CLASSPATH=%CLASSPATH%;../lib/commons-lang-2.4.jar
-set CLASSPATH=%CLASSPATH%;../lib/commons-io-1.4.jar
-
-java -classpath %CLASSPATH% com.googlecode.jsendnsca.Main %*
